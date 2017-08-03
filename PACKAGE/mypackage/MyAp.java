b46469bfd867e9215498ed41bf4a7a713917415e
Tsuru <tokyo.tokyobanana@gmail.com>
2017-08-04 02:20:41 +0900

    PACKAGE 完了!!

---
+++ PACKAGE/mypackage/MyAp.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class MyAp{
+	public MyAp(){
+		System.out.println("Invoke mypackage.MyAp!");
+	}
+	public void print(){
+		System.out.println("mypackage.MyAp.print()!");
+	}
+}
